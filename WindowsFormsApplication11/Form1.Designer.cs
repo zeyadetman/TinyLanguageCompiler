de2df243d49861fc5c719c786cdddca022147c84
Zeyad Etman <zeyadetman@gmail.com>
2017-10-17 02:44:13 +0200

    add all files

---
+++ WindowsFormsApplication11/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApplication11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lexeme = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tokenClass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(276, 274);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Compile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 322);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(583, 82);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Token Classes";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tiny Code";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.lexeme,
+            this.tokenClass});
+            this.dataGridView1.Location = new System.Drawing.Point(297, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(303, 237);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lexeme
+            // 
+            this.lexeme.HeaderText = "Lexeme";
+            this.lexeme.MinimumWidth = 10;
+            this.lexeme.Name = "lexeme";
+            this.lexeme.Width = 130;
+            // 
+            // tokenClass
+            // 
+            this.tokenClass.HeaderText = "Token Class";
+            this.tokenClass.MinimumWidth = 10;
+            this.tokenClass.Name = "tokenClass";
+            this.tokenClass.Width = 130;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Error List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 416);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Compiler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lexeme;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tokenClass;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication11/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication11
+{
+    public partial class Form1 : Form
+    {   
+        public string tinyCode = "";
+        public string[] rsrvdwrds = {"int","float","string","read","write","repeat","until","if","elseif","else","then","return","endl"};
+        Dictionary<string, string> tokens = new Dictionary<string, string>()
+        {
+            {"main","Main_Function"},{"int","Datatype"},{"float","Datatype"},{"string","Datatype"},{":=","AssignmentOperator"},
+            {"write","Write_Statement"},{"read","Read_Statement"},{"return","Return_Statement"},{"repeat","Repeat_Statement"},
+            {"program","Program"},{"if","If_Statement"},{"elseif","Else_If_Statement"},{"else","Else_Statement"},{"<>","not equal"}
+        };
+        Dictionary<char, string> operators = new Dictionary<char, string>(){
+            {'+',"PlusOperator"},{'-',"MinusOperator"},{'*',"MultiplicationOperator"},{';',"Semicolon"},{'<',"less than"},
+            {'>',"greater than"},{'=',"is equal"}
+        };
+
+        public void extract(string tinyCodeLocal, Dictionary<string, string> tokensLocal, Dictionary<char,string> operatorsLocal)
+        {
+            string lastWord = "";
+            string savedWord = "";
+            for (int i = 0; i < tinyCodeLocal.Length; i++)
+            {
+                if (Char.IsDigit(tinyCodeLocal[i]))
+                {
+                    savedWord += tinyCodeLocal[i];
+                    for (int j = ++i; j < tinyCodeLocal.Length; j++)
+                    {
+                        if (Char.IsDigit(tinyCodeLocal[j]) || tinyCodeLocal[j] == '.')
+                        {
+                            savedWord += tinyCodeLocal[j];
+                        }
+                        else if (operatorsLocal.ContainsKey(tinyCodeLocal[j])) {
+                            i = j;
+                            dataGridView1.Rows.Add(savedWord, "Number");
+                            dataGridView1.Rows.Add(tinyCodeLocal[j], operatorsLocal[tinyCodeLocal[j]]);
+                            savedWord = "";
+                            break;
+                        }
+                        else
+                        {
+                            i = j;
+                            dataGridView1.Rows.Add(savedWord, "Number");
+                            lastWord = "Number";
+                            savedWord = "";
+                            break;
+                        }
+                    }
+                }  
+
+                else if (Char.IsLetter(tinyCodeLocal[i])) {
+                    savedWord += tinyCodeLocal[i];
+                    for (int j = ++i; j < tinyCodeLocal.Length; j++)
+                    {
+                        if (Char.IsLetter(tinyCodeLocal[j]) || Char.IsDigit(tinyCodeLocal[j])) { savedWord += tinyCodeLocal[j]; }
+                        else if (tinyCodeLocal[j] == '(')
+                        {
+                            i = j;
+                            if (tokensLocal.ContainsKey(lastWord))
+                            {
+                                if (tokensLocal[lastWord] == "Datatype")
+                                {
+                                    if (!tokensLocal.ContainsKey(savedWord)) dataGridView1.Rows.Add(savedWord, "FunctionName");
+                                    else dataGridView1.Rows.Add(savedWord, tokensLocal[savedWord]);
+                                    lastWord = savedWord;
+                                    savedWord = "";
+                                    break;
+                                }
+                                else
+                                {
+                                    dataGridView1.Rows.Add(savedWord, "Function_Call");
+                                    lastWord = savedWord;
+                                    savedWord = "";
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                dataGridView1.Rows.Add(savedWord, "Function_Call");
+                                lastWord = savedWord;
+                                savedWord = "";
+                                break;
+                            }
+
+                        }
+                        else if (tinyCodeLocal[j] == ';')
+                        {
+                            if (!tokensLocal.ContainsKey(savedWord)) dataGridView1.Rows.Add(savedWord, "Identifier");
+                            else dataGridView1.Rows.Add(savedWord, tokensLocal[savedWord]);
+                            i = j; 
+                            dataGridView1.Rows.Add(";", operatorsLocal[';']);
+                            lastWord = savedWord;
+                            savedWord = "";
+                            break;
+                        }
+                        else if (operatorsLocal.ContainsKey(tinyCodeLocal[j]))
+                        {
+                            i = j;
+                            dataGridView1.Rows.Add(savedWord, "Number");
+                            dataGridView1.Rows.Add(tinyCodeLocal[j], operatorsLocal[tinyCodeLocal[j]]);
+                            savedWord = "";
+                            break;
+                        }
+                        else if (tinyCodeLocal[j] == ':' && tinyCodeLocal[j + 1] == '=')
+                        {
+                            if (!tokensLocal.ContainsKey(savedWord)) dataGridView1.Rows.Add(savedWord, "Identifier");
+                            else dataGridView1.Rows.Add(savedWord, tokensLocal[savedWord]);
+                            j++; i = j; dataGridView1.Rows.Add(":=", tokensLocal[":="]); lastWord = ":=";
+                            savedWord = "";
+                            break;
+                        }
+                        else
+                        {
+                            i = j;
+                            if (!tokensLocal.ContainsKey(savedWord)) dataGridView1.Rows.Add(savedWord, "Identifier");
+                            else dataGridView1.Rows.Add(savedWord, tokensLocal[savedWord]);
+                            lastWord = savedWord;
+                            savedWord = "";
+                            break;
+                        }
+                    }
+                }
+                else if (tinyCodeLocal[i] == '"') {
+                    for (int j = ++i; j < tinyCodeLocal.Length; j++)
+                    {
+                        if (tinyCodeLocal[j] != '"') { savedWord += tinyCodeLocal[j]; }
+                        else
+                        {
+                            i = j;
+                            dataGridView1.Rows.Add(savedWord, "String");
+                            lastWord = savedWord;
+                            savedWord = "";
+                            break;
+                        }
+                    }
+                }
+                else if (tinyCodeLocal[i] == ';') { dataGridView1.Rows.Add(";", operatorsLocal[';']); }
+                else if (operatorsLocal.ContainsKey(tinyCodeLocal[i]))
+                {
+                    dataGridView1.Rows.Add(tinyCodeLocal[i], operatorsLocal[tinyCodeLocal[i]]);
+                    savedWord = "";
+                    break;
+                }
+                else if (tinyCodeLocal[i] == ':' && tinyCodeLocal[i + 1] == '=') { i++; dataGridView1.Rows.Add(":=", tokensLocal[":="]); }
+                else if (tinyCodeLocal[i] == '/' && tinyCodeLocal[i + 1] == '*')
+                {
+                    i += 2;
+                    for (int j = i; j < tinyCodeLocal.Length; j++)
+                    {
+                        if (tinyCodeLocal[j] == '*' && tinyCodeLocal[j + 1] == '/')
+                        {
+                            i = j + 1;
+                            dataGridView1.Rows.Add(savedWord, "Comment");
+                            lastWord = savedWord;
+                            savedWord = "";
+                            break;
+                        }
+                        else savedWord += tinyCodeLocal[j];
+                    }
+
+                }
+                
+                
+
+            }
+
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Refresh the dataGridView
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            //--------------------------
+
+            //read tinyCode from user and extract values 
+            tinyCode = richTextBox1.Text;
+            extract(tinyCode, tokens, operators);
+            //-----------------------------------------
+        }
+
+        private void label1_Click(object sender, EventArgs e){}
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
